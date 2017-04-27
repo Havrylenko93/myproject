9d39a85167750c314d59325f0466164028117ef8
sergej.gavrilenko <gavrilenko.php@gmail.com>
2017-04-27 11:05:45 +0000

    work with code stable

---
+++ public/doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "likulator",
+  "version": "1.0.0",
+  "description": "likulator description",
+  "title": "likulator",
+  "url": "http://ec2-54-229-150-116.eu-west-1.compute.amazonaws.com/api/v1/",
+  "sampleUrl": "http://ec2-54-229-150-116.eu-west-1.compute.amazonaws.com/api/v1/",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-04-26T14:18:48.066Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
